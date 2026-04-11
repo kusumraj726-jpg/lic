c07145398fec030364890a6fc53bc4a0795900f9
Shivam-sharma23 <shivam.sh0023@gmail.com>
2026-04-11 23:39:46 +0530

    Enable auto-migrations on startup

--- docker/start.sh
+++ docker/start.sh
@@ -3,8 +3,8 @@
 # Start PHP-FPM in the background
 php-fpm -D
 
-# Run migrations (optional, can be done manually or via deployment hooks)
-# php artisan migrate --force
+# Run migrations
+php artisan migrate --force
 
 # Start Nginx in the foreground
 nginx -g "daemon off;"
