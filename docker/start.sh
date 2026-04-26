b3b1341df7efc9270eaa0e63164cfed4cc677dad
Shivam-sharma23 <shivam.sh0023@gmail.com>
2026-04-26 09:35:06 +0530

    Fix storage permissions

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Ensure storage and cache directories are writable by the web server
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Start PHP-FPM in the background
 php-fpm -D
 
