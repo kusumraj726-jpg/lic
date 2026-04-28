db4074a591b3a8a3424c4c794e39de0d5e72ab7c
Shivam-sharma23 <shivam.sh0023@gmail.com>
2026-04-28 22:34:29 +0530

    Fix missing favicon on login/auth pages by adding it to auth-split-layout

--- resources/views/components/auth-split-layout.blade.php
+++ resources/views/components/auth-split-layout.blade.php
@@ -8,6 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon-nb.png') }}">
 
     <title>{{ config('app.name', 'NexoraByte') }} - {{ $title ?? 'Auth' }}</title>
 
